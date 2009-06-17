3106fd525365b260aa0009c89c39f3719c114559
Shekhar Tiwatne <world.tech@the-hub.net>
2009-06-17 06:51:17 +0530

    move one level up

--- hubspace/setup.py
+++ hubspace/setup.py
@@ -1,46 +1,26 @@
 from setuptools import setup, find_packages
-#from turbogears.finddata import find_package_data
 import sys, os
 
 version = '1.0'
 
 setup(name='hubspace',
       version=version,
-      description="Hub space management system",
+      description="hub space management",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='hub space mangement',
+      keywords='hub space management',
       author='tom salfield',
       author_email='tom.salfield@the-hub.net',
       url='the-hub.net',
-      license='GPL',
-      packages=find_packages(),
-      #package_data = find_package_data(where='hubspace',
-      #                                 package='hubspace'),
+      license='GPLV2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-           "TurboGears == 1.0.7",
-           "SQLObject == 0.10.2",
-           "formencode >= 0.7.1",
-           "PIL",
-           "smbpasswd",
-           "syncer", 
-	   "kid",
-       	   "docutils",
-           "TurboFeeds",
-           "TurboLucene",
-           "funkload",
-           "pytz",
-           "pycountry",
-	   "nose",
-           "BeautifulSoup"
+          # -*- Extra requirements: -*-
       ],
       entry_points="""
-[console_scripts]
-run_hubspace = start_hubspace:main
-export2ldap  = export2ldap:main
+      # -*- Entry points: -*-
       """,
-      test_suite = 'nose.collector',
       )
