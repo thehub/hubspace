c51ef82e480b118d079c0d5dda174117ee3a83d0
Shekhar Tiwatne <world.tech@the-hub.net>
2009-09-09 05:17:37 +0530

    Need to move and rename later

---
+++ buildpyjamas.py
@@ -0,0 +1,39 @@
+import os
+import glob
+import sys
+from hubspace.utilities.static_files import minify
+
+jsbuilder = os.path.abspath("./bin/pyjsbuild")
+srcdir = "hubspace/fe/src"
+outdir = os.path.abspath("hubspace/static/fe")
+file_types_to_minify = ['js', 'css', 'html']
+build_cmd = "%(jsbuilder)s -d -m -O --print-statement -o %(outdir)s %%(src)s" % locals()
+
+if not os.path.isdir(outdir):
+    os.makedirs(outdir)
+
+def compilePyjSources():
+    dir0 = os.getcwd()
+    
+    os.chdir(srcdir)
+    for src in glob.glob("*.py"):
+        if not "RootPanel" in file(src).read(): # Assumption is a src file with no RootPanel in it, most likely does not need direct compilation 
+            continue
+        cmd = build_cmd % locals()
+        if not os.system(cmd) == 0:
+            sys.exit("failed: %s" % cmd)
+    
+    os.chdir(outdir)
+    for root, dirs, files in os.walk('.'):
+        for name in files:
+            path = os.path.join(root, name)
+            ext = name.split('.')[-1]
+            if ext in file_types_to_minify:
+                print "minifying ", path
+                out = minify(path)
+                file(path, 'w').write(out)
+    
+    os.chdir(dir0)
+
+if __name__ == "__main__":
+    compilePyjSources()
---
+++ installpyjamas.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+thispython = sys.executable
+pyjversion = "pyjamas-0.6"
+archive = "%(pyjversion)s.tgz" % locals()
+url = "http://pypi.python.org/packages/source/P/Pyjamas/%(archive)s" % locals()
+
+dir0 = os.getcwd()
+bindir = os.path.join(dir0, "bin")
+pyjamasdst = "../eggs"
+
+def install():
+    os.chdir(pyjamasdst)
+    
+    commands = [
+        "wget %s" % url,
+        "tar zxvf %s" % archive,
+        "cd %(pyjversion)s && %(thispython)s bootstrap.py" % locals(),
+        "cp -v %(pyjversion)s/bin/* %(bindir)s" % locals()
+        ]
+    for cmd in commands:
+        print cmd
+        if os.system(cmd) != 0:
+            sys.exit("%(cmd)s command failed" % locals())
+    
+    os.chdir(dir0)
