bc23efaee814e2fdb5056bd19f841161991894c1
Admin Tom Salfield <admin-ts@space-hub.nile.the-hub.net>
2010-04-14 23:43:22 +0200

    image helpers changes

--- hubspace/utilities/image_helpers.py
+++ hubspace/utilities/image_helpers.py
@@ -11,6 +11,11 @@ def image_src(object, attr_name, default_image_location):
    if isinstance(getattr(object, attr_name), basestring) and isinstance(get_mimetype(object, attr_name), basestring):
        count = cherrypy.session.setdefault('count', 1000)
        cherrypy.session['count'] +=1 
-       #return "/display_image/"+ object.__class__.__name__ +"/" + str(object.id) + "/" + attr_name
-   return "http://plus.the-hub.net/site_media/avatars/%(user_name)s/resized/180/avatars/%(user_name)s/%(user_name)s" % \
-          dict(user_name = object.user_name)
+       obj_type = object.__class__.__name__
+       return "/display_image/"+ obj_type +"/" + str(object.id) + "/" + attr_name
+   return default_image_location
+   # hub+ avatar integration code
+   #     if not obj_type == 'User':
+   #         return "/display_image/"+ obj_type +"/" + str(object.id) + "/" + attr_name
+   # return "http://plus.the-hub.net/site_media/avatars/%(user_name)s/resized/180/avatars/%(user_name)s/%(user_name)s" % \
+   #        dict(user_name = object.user_name)
