04a3782cd13c9786f2cf5bb8d521726f78326393
Admin Shekhar Tiwatne <admin-st@spacedev.volga.the-hub.net>
2010-05-27 10:38:33 +0000

    #675

--- hubspace/utilities/users.py
+++ hubspace/utilities/users.py
@@ -84,8 +84,10 @@ def filter_members(location, text_filter, type, active_only, start, end, overrid
 
         if user_locs:
             if location:
+                relevant_groups = location.groups
+                relevant_user_ids = tuple((ug.userID for ug in UserGroup.select(IN(UserGroup.q.group, tuple(relevant_groups)))))
                 display_name_clause = iLIKE(User.q.display_name, text_filter)
-                location_clause = (User.q.location == location)
+                user_id_clause = IN(User.q.id, relevant_user_ids) # so that we select all members even those with homehub as this hub
                 if active_only:
                     user_active_clause = (User.q.active == 1)
                     users = User.select(AND(display_name_clause, user_id_clause, user_active_clause))
