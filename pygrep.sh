6bed762eaf3fb3ee17a973ca0ec7323ef5256b4b
Admin Shekhar Tiwatne <admin-st@space-hub.nile.the-hub.net>
2011-07-05 09:58:40 +0200

    included hub.js

--- pygrep.sh
+++ pygrep.sh
@@ -3,6 +3,8 @@ if [ $1 ]
 echo "Searching for \"$1\""
 then
     find . -name '*.py' -type f -exec egrep -i -H "$1" '{}' \; | more
+    find . -name '*.kid' -type f -exec egrep -i -H "$1" '{}' \; | more
+    find . -name 'hub.js' -type f -exec egrep -i -H "$1" '{}' \; | more
 else
     echo "Usage: $0 pattern"
 fi
